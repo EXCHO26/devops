4f2deb317cdde31f5812d0506dc4a55c203ec6e8
Krasimir Peev <krasimirpeev2003@gmail.com>
2026-01-17 19:30:14 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World"
+
+@app.route("/hostname")
+def hostname():
+    return f"Hostname: {socket.gethostname()}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
